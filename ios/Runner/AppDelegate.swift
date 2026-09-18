d51672b9948b5edda2b3c7c479e00e3070364632
Stanislav Popov <hashhtagg@yandex.ru>
2026-09-19 04:27:10 +0800

    fix: add Yandex Maps API key initialization in AppDelegate.swift for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import UserNotifications
+import YandexMapsMobile
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +9,27 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // ✅ Yandex MapKit API Key
+    YMKMapKit.setApiKey("56984492-3dbc-4c22-8b0f-03790464b774")
+    YMKMapKit.sharedInstance()
+    
+    // ✅ Push Notifications
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: { _, _ in }
+      )
+    } else {
+      let settings: UIUserNotificationSettings =
+        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
+    }
+    
+    application.registerForRemoteNotifications()
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
